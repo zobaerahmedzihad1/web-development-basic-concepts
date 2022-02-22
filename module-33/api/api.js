22ae8f0654301475f36688bde1e86d1463546ac1
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-02-22 20:02:48 +0600

    Load data from api and display it daynamically.

--- module-33/api/api.js
+++ module-33/api/api.js
@@ -3,3 +3,24 @@ function loadTodo() {
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
+
+// load users
+function loadUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => displayUsers(data));
+}
+function displayUsers(data) {
+  const ul = document.getElementById("users");
+  for (const users of data) {
+    const li = document.createElement("li");
+    li.innerText = `Name: ${users.name} And Email: ${users.email}`;
+    ul.appendChild(li);
+  }
+}
+// load posts
+function loadPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
