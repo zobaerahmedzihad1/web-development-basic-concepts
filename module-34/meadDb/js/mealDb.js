226cd57413c5363793f2045340bf8d4496e5e2ee
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-02-24 23:48:20 +0600

    Create dynamic url based on click and display data

--- module-34/meadDb/js/mealDb.js
+++ module-34/meadDb/js/mealDb.js
@@ -13,14 +13,14 @@ const searchFood = () => {
 const displayResult = (meals) => {
   const searchResult = document.getElementById("search-result");
   meals.forEach((meal) => {
-    console.log(meal);
+    // console.log(meal);
 
-    const { strMealThumb, strMeal, strInstructions } = meal;
+    const { strMealThumb, strMeal, strInstructions, idMeal } = meal;
 
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-      <div class="card h-100">
+      <div onclick = loadDetails('${idMeal}') class="card h-100">
           <img width="100px" src="${strMealThumb}" class="card-img-top" alt="image" />
           <div class="card-body">
             <h5 class="card-title">${strMeal}</h5>
@@ -35,4 +35,17 @@ const displayResult = (meals) => {
     `;
     searchResult.appendChild(div);
   });
+};
+
+// Create dynamic url based on click and display data
+const loadDetails = (meals) => {
+  console.log(meals);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meals}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+
+const displayMealDetails = (meal) => {
+  console.log(meal);
 };
