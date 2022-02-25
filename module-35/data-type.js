84b8fc8ba7274b4186a24b60b86593ee5ade4548
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-02-25 11:24:58 +0600

    Check Dynamic data types

---
+++ module-35/data-type.js
@@ -0,0 +1,14 @@
+// Number data type variable
+// let a = 7;
+// console.log(typeof a);
+
+// string data type variable
+let a = "check my data type";
+console.log(typeof a);
+
+// boolean data type variable
+const boolean = true;
+console.log(typeof boolean);
+
+// javascript is a dynamically type programming language. It data type depends on the right side what type of value
+//  we written.
