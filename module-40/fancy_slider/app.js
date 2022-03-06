c1c63213b963dc5aaba172ddff1333c0a6f3b23f
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-07 00:19:43 +0600

    More debug to fix slider to slide by getting duration

--- module-40/fancy_slider/app.js
+++ module-40/fancy_slider/app.js
@@ -68,7 +68,7 @@ const createSlider = () => {
   document.querySelector(".main").style.display = "block";
   // hide image aria
   imagesArea.style.display = "none";
-  const doration = document.getElementById("duration").value || 1000;
+  const duration = document.getElementById("duration").value || 1000;
   sliders.forEach((slide) => {
     let item = document.createElement("div");
     item.className = "slider-item";
