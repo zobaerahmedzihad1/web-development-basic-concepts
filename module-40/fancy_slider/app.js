1e2cf72aa0923cb1bedb3eab6083ef2dc5bfd366
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-07 00:36:24 +0600

    Fix negative duration and how to read others code

--- module-40/fancy_slider/app.js
+++ module-40/fancy_slider/app.js
@@ -69,6 +69,12 @@ const createSlider = () => {
   // hide image aria
   imagesArea.style.display = "none";
   const duration = document.getElementById("duration").value || 1000;
+
+  if (duration < 0) {
+    alert("Please give a positive value.");
+    return;
+  }
+
   sliders.forEach((slide) => {
     let item = document.createElement("div");
     item.className = "slider-item";
