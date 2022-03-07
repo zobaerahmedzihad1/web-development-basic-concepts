8f989b15d2035546012ec525bc49d96e8c1a226f
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-08 01:24:33 +0600

    arrow function

---
+++ module-42/arrow_function.js
@@ -0,0 +1,15 @@
+const getFiftyFive = () => 55;
+const add = () => 55 + 37;
+
+const isEven = (num) => num % 2 == 0;
+console.log(isEven(7));
+
+const add2 = () => 7 + 5 + 1;
+console.log(add2());
+
+// multi line arrow function
+const addition = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+};
+console.log(addition(7, 13));
