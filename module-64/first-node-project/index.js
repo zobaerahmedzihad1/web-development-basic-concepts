7b2f4195041b7371106f5e9e428bd90113814d1c
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-20 08:18:34 +0600

    create dynamic route and access user dynamically

--- module-64/first-node-project/index.js
+++ module-64/first-node-project/index.js
@@ -6,8 +6,22 @@ app.get("/", (req, res) => {
   res.send("I am from backend.");
 });
 
-app.get("/friend", (req, res) => {
-  res.send("See all my friends.");
+const friends = [
+  { id: 1, name: "masum", phone: "0124535717" },
+  { id: 2, name: "sahin", phone: "0124563214" },
+  { id: 3, name: "bellal", phone: "012456698" },
+  { id: 4, name: "shiplu", phone: "012456564" },
+  { id: 5, name: "shiplu", phone: "012456832" },
+];
+
+app.get("/friends", (req, res) => {
+  res.send(friends);
+});
+
+app.get("/friend/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const friend = friends.find((friend) => friend.id === id);
+  res.send(friend);
 });
 
 app.listen(port, () => {
