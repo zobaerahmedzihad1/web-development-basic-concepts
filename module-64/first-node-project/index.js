e3d497f62fbe98e485e093ef18d8919b189e96b6
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-22 00:22:46 +0600

    create mongo db atlus and connect to database

--- module-64/first-node-project/index.js
+++ module-64/first-node-project/index.js
@@ -1,43 +1,34 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 4700;
+const port = process.env.PORT || 5000;
 
+// user : dbuser1
+// password: kosHtiJLM0PJnfln
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://dbuser1:kosHtiJLM0PJnfln@cluster0.w5uoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("db connected.");
+  client.close();
+});
+
 app.get("/", (req, res) => {
-  res.send("I am from backend.");
-});
-
-const friends = [
-  { id: 1, name: "masum", phone: "0124535717" },
-  { id: 2, name: "sahin", phone: "0124563214" },
-  { id: 3, name: "bellal", phone: "012456698" },
-  { id: 4, name: "sahadat", phone: "012456564" },
-  { id: 6, name: "sanaul", phone: "012456832" },
-  { id: 7, name: "rabiul", phone: "012456832" },
-  { id: 8, name: "salman", phone: "012456832" },
-  { id: 9, name: "rakibul", phone: "012456832" },
-];
-
-app.get("/friends", (req, res) => {
-  res.send(friends);
-});
-
-app.get("/friend/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const friend = friends.find((friend) => friend.id === id);
-  res.send(friend);
-});
-
-app.post("/friends", (req, res) => {
-  const friend = req.body;
-  friend.id = friends.length + 2;
-  friends.push(friend)
-  res.send("friend")
+  res.send("I Am from the server.");
 });
 
 app.listen(port, () => {
-  console.log("Listening from ", port);
+  console.log("listening From ", port);
 });
