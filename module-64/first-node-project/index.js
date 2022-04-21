03ca5c8a24aa03bd296d638689474d4b24308152
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-22 03:45:41 +0600

    Load and display data from database, create delete button

--- module-64/first-node-project/index.js
+++ module-64/first-node-project/index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const res = require("express/lib/response");
@@ -25,23 +25,29 @@ async function run() {
     await client.connect();
     const userCollection = client.db("foodExpress").collection("user");
 
-    // read data from mongodb database.
-    app.get("/users", async (req, res) => {
+    // get users
+    app.get("/user", async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query);
       const users = await cursor.toArray();
       res.send(users);
     });
 
-    // receive data from client side.
+    // post: add new a user.
     app.post("/user", async (req, res) => {
-      const user = req.body;
-      console.log(user);
+      const newUser = req.body;
+      console.log(newUser);
       // send data to mongodb.
-      const result = await userCollection.insertOne(user);
+      const result = await userCollection.insertOne(newUser);
       console.log(`Data was inserted to mongodb ${result.insertedId}`);
-
       res.send(result);
+    });
+
+    // Delete user.
+
+    app.delete("/user/:id", (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
     });
   } finally {
     // await client.close()
