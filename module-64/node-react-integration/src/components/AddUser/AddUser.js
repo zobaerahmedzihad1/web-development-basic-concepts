4def2d78a3f9d4b909fd3ecbc5c00d291e8987c9
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-22 02:09:12 +0600

    Send user data to the server and get response to client

--- module-64/node-react-integration/src/components/AddUser/AddUser.js
+++ module-64/node-react-integration/src/components/AddUser/AddUser.js
@@ -1,9 +1,36 @@
 import React from "react";
 
 const AddUser = () => {
+  const handleUserInformation = (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    const user = { name, email };
+
+// send data to server (backend)
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
-    <div>
-      <h2>Please add user.</h2>
+    <div style={{ textAlign: "center" }}>
+      <h3>Please add a new user.</h3>
+      <form onSubmit={handleUserInformation}>
+        <input type="text" placeholder="Name" name="name" required />
+        <br />
+        <input type="text" placeholder="Email" name="email" required />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
     </div>
   );
 };
