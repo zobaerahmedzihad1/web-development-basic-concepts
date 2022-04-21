6a574d05e2988db184f90b55c8517d968120a3f5
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-21 12:42:00 +0600

    receive data from client side and send data

--- module-64/first-node-project/index.js
+++ module-64/first-node-project/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4700;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("I am from backend.");
@@ -10,8 +14,11 @@ const friends = [
   { id: 1, name: "masum", phone: "0124535717" },
   { id: 2, name: "sahin", phone: "0124563214" },
   { id: 3, name: "bellal", phone: "012456698" },
-  { id: 4, name: "shiplu", phone: "012456564" },
-  { id: 5, name: "shiplu", phone: "012456832" },
+  { id: 4, name: "sahadat", phone: "012456564" },
+  { id: 6, name: "sanaul", phone: "012456832" },
+  { id: 7, name: "rabiul", phone: "012456832" },
+  { id: 8, name: "salman", phone: "012456832" },
+  { id: 9, name: "rakibul", phone: "012456832" },
 ];
 
 app.get("/friends", (req, res) => {
@@ -22,6 +29,13 @@ app.get("/friend/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const friend = friends.find((friend) => friend.id === id);
   res.send(friend);
+});
+
+app.post("/friends", (req, res) => {
+  const friend = req.body;
+  friend.id = friends.length + 2;
+  friends.push(friend)
+  res.send("friend")
 });
 
 app.listen(port, () => {
